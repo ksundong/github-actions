10e04dbfa8b840fd52932bad168dc8bde740fbe0
IdionKim <ksundong@gmail.com>
2020-05-22 23:33:42 +0900

    auto deploy 해보기

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/testapp
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
+APP_NAME=airbnb-11
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
